6462ef3bc2d7da949384895b62226cb5311403e9
Brian Nelson <brian@treegecko.com>
2018-03-29 22:06:17 -0600

    Work in progress.

--- SidToS3/data/datafile.py
+++ SidToS3/data/datafile.py
@@ -19,14 +19,30 @@ class DataFile:
         for line in lines:
             if line.startswith("#"):
                 if line.startswith("# Site = "):
-                    self.Site = line[7:13]
+                    self.Site = line[8:-1]
                 elif line.startswith("# StationID = "):
-                    self.StationId = line[12:15]
+                    self.StationId = line[12:-1]
                 elif line.startswith("# Contact = "):
-                    self.Contact = line[10]
+                    self.Contact = line[10:-1]
+                elif line.startswith("# Longitude = "):
+                    self.Longitude = float(line[13:-1])
+                elif line.startswith("# Latitude = "):
+                    self.Latitude = float(line[12:-1])
+                elif line.startswith("# Country = "):
+                    self.Country = line[11:-1]
+                elif line.startswith("# SampleRate = "):
+                    self.SampleRate = line[14:-1]
+                elif line.startswith("# Frequency = "):
+                    self.Frequency = line[13:-1]
+                elif line.startswith("# MonitorId = "):
+                    self.MonitorId = line[13:-1]
+                elif line.startswith("# UTC_StartTime = "):
+                    self.UtcStartTime = datetime.strptime(line[18:-1], "%Y-%m-%d %H:%M:%S")
+                elif line.startswith("# UTC_Offset = "):
+                    self.UtcOffset = line[14:-1]
             else:
                 date_string = line[0:19]
-                value_string = line[21:]
+                value_string = line[21:-1]
 
                 date_value = datetime.strptime(date_string, "%Y-%m-%d %H:%M:%S")
                 value = float(value_string)
--- SidToS3/workers/chartbuilder.py
+++ SidToS3/workers/chartbuilder.py
@@ -1,8 +1,12 @@
+from datetime import timedelta
+from datetime import datetime
+from datetime import date
 from data.datafile import DataFile
-import os
-import cairosvg
-import pygal
-
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+from astral import Astral
+from astral import Location
 
 class ChartBuilder:
 
@@ -14,18 +18,55 @@ class ChartBuilder:
     def generate_chart(self, data_filename):
         datafile = DataFile(data_filename)
 
-        #x = []
-        #y = []
-        series = []
+        x = []
+        y = []
 
         for dp in datafile.DataPoints:
-            #x.append(dp.ReadingTime)
-            #y.append(dp.ReadingValue)
-            series.append((dp.ReadingTime, dp.ReadingValue))
+            time = self.get_decimal_time(dp.ReadingTime)
 
-        chart = pygal.DateTimeLine()
-        chart.add("readings", series)
-        chart.render_to_png(filename='/Transfer/SuperSid/Temp/test.png')
+            x.append(time)
+            y.append(dp.ReadingValue)
+
+        sun = self.get_sun_info(datafile)
+
+        sunset = self.get_decimal_time(sun[0])
+        sunrise = self.get_decimal_time(sun[1])
+
+        plt.plot(x, y)
+        plt.xlim(0, 24)
+        plt.xlabel("UTC Time")
+        plt.ylabel("Signal Strength")
+
+        current_date = datafile.UtcStartTime.strftime("%Y-%m-%d")
+        plt.title("{0}\n{1}".format(datafile.Site, current_date))
+        plt.axvspan(sunset, sunrise, facecolor='blue', alpha=0.2)
+        plt.savefig('/Transfer/SuperSid/Temp/test.png')
 
         return
+
+    @staticmethod
+    def get_decimal_time(datetime_value):
+        return datetime_value.hour \
+               + datetime_value.minute / 60 \
+               + datetime_value.second / 3600
+
+    @staticmethod
+    def get_sun_info(datafile):
+
+        site = Location()
+        site.name = datafile.Site
+        site.region = datafile.Country
+        site.latitude = datafile.Latitude
+        site.longitude = datafile.Longitude
+        site.elevation = 0
+
+        current_day = datafile.UtcStartTime.date()
+        sun = site.sun(date=current_day)
+        sunrise = sun['sunrise']
+
+        previous_day = current_day - timedelta(days=1)
+        sun = site.sun(date=previous_day)
+        sunset = sun['sunset']
+
+        return sunset, sunrise
 
